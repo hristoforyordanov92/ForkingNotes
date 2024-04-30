08364667e4c01e3e8e4a7943daa8d67181dcca3c
Hristofor Yordanov <hristofor.yordanov.92@gmail.com>
2024-04-30 13:48:56 +0300

    Loosen up the tag validations.
    - Tags will no longer have strict validation, which only allows letters, numbers and a dash symbol. Now everything is allowed, except for a tag consisting of only empty spaces.

--- NotesApp/Validations/TagValidation.cs
+++ NotesApp/Validations/TagValidation.cs
@@ -1,25 +1,16 @@
 ﻿using System.Globalization;
-using System.Text.RegularExpressions;
 using System.Windows.Controls;
 
 namespace NotesApp.Validations
 {
     public partial class TagValidation : ValidationRule
     {
-        private static readonly Regex _tagRegex = TagRegex();
-
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            if (value is not string tag || string.IsNullOrEmpty(tag))
+            if (value is not string tag || string.IsNullOrWhiteSpace(tag))
                 return new ValidationResult(false, "Invalid tag");
-
-            if (_tagRegex.Match(tag).Success)
-                return new ValidationResult(false, "Tag must contain only lowercase letters, digits and -.");
 
             return ValidationResult.ValidResult;
         }
-
-        [GeneratedRegex(@"[^a-z0-9\-]")]
-        private static partial Regex TagRegex();
     }
 }
